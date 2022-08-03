f104980b921d23bbd2b7f09bc0433f26a9c47355
y-arjun-y <arjyadav7@gmail.com>
2022-08-03 09:15:00 +0400

    changed to httpEquiv

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function ArjunYadav({ Component, pageProps }) {
     <>
       <Head>
         <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:locale" content="en" />
         <meta
