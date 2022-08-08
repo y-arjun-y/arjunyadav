7db5d6856cab2bd623fa1a129717d07463dd0c79
y-arjun-y <arjyadav7@gmail.com>
2022-08-08 09:24:59 +0400

    changed navbar folder position

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Navbar from "./components/Navbar";
+import Navbar from "/components/Navbar";
 import "highlight.js/styles/monokai-sublime.css";
 
 function ArjunYadav({ Component, pageProps }) {
