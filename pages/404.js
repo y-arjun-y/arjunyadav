84259621a2e7959f7edcbae25b3c162b97710e75
y-arjun-y <arjyadav7@gmail.com>
2023-01-26 18:22:15 +0400

    26/01/2023 - added resume

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,6 @@ export default function Custom404() {
   return (
     <>
       <h3>404 - Sorry, but this page does not exist.</h3>
-      <h5></h5>
       <h6>
         Click <a href="/">here</a> to return back to the home page.
       </h6>
---
+++ pages/resume.js
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <>
+      <embed src="/resume.pdf" width="800px" height="2100px" />
+    </>
+  );
+}
