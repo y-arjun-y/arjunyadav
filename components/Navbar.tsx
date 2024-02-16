135501a2b6ccf68921a6a899a329a71bc620faa3
y-arjun-y <arjyadav7@gmail.com>
2024-02-16 22:48:11 +0400

    fixed navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,20 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <header>
-      <Link href="/">
-        <h1>Bear Blog Replacement Template</h1>
-      </Link>
+      <Link href="/" passHref>
+        <h1>Arjun Yadav</h1>
+      </a>
       <nav>
         <p>
           <Link href="/">Home</Link>
           <Link href="/blog/">Blog</Link>
-          <Link href="/the-grizzly-bear-chill-zone/">
-            The Grizzly Bear Chill Zone
-          </Link>
+          <Link href="/series">Series</Link>
+          <Link href="/projects/">Projects</Link>
+          <br />
+          <Link href="/notebook">Notebook</Link>
+          <Link href="/profiles">Profiles</Link>
+          <Link href="/resume">Résumé</Link>
+          <Link href="/misc/">Misc.</Link>
         </p>
       </nav>
     </header>
