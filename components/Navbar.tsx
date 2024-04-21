bb2bdbce9b2199495cb0812a3ba547147443afea
y-arjun-y <arjyadav7@gmail.com>
2024-04-21 17:23:16 +0400

    21/04/2024 - project logs update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       <nav>
         <p>
           <Link href="/">Home</Link>
-          <Link href="/blog/">Blog (<Link href="https://yarjuny.substack.com/subscribe">subscribe!</Link>)</Link>
+          <span><Link href="/blog/">Blog</Link>(<Link href="https://yarjuny.substack.com/subscribe">subscribe!</Link>)</span>
           <Link href="/series">Series</Link>
           <Link href="/projects/">Projects</Link>
           <br />
